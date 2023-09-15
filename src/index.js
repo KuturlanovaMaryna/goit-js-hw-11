c9359d8c313057d48de8de2353b36c261e507f27
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-09-15 20:56:57 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import "./css/styles.css"
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
+import { fetchImeges } from "./js/pixabay-api";
+
+refs = {
+    searchForm: document.querySelector(".search-form"),
+    searchSubmitBtn: document.querySelector(".search-btn"),
+    inputForm: document.querySelector(".input-form"),
+}
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,5 +3,14 @@ import axios from "axios";
 const API_KEY = "39422970-7b6782b5257085f988eb60a82";
 
 const BASE_URL = "https://pixabay.com/api/"
+
     
-axios.defaults.headers.common["x-api-key"] = API_KEY;
+export async function fetchImeges(value) {
+    try {
+        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`)
+        return response;
+    }
+    catch (error) {
+    console.log(error);
+  }
+}
