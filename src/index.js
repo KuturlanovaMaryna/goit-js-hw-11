cd88b816350f88c22118fc9e8dbb03c24b043758
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-09-16 16:26:42 +0300

    update homework

--- src/index.js
+++ src/index.js
@@ -2,10 +2,142 @@ import "./css/styles.css"
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { fetchImeges } from "./js/pixabay-api";
+import { fetchImages } from "./js/pixabay-api";
 
 refs = {
     searchForm: document.querySelector(".search-form"),
     searchSubmitBtn: document.querySelector(".search-btn"),
     inputForm: document.querySelector(".input-form"),
+    galleryImages: document.querySelector(".gallery"),
+    loadMoreBtn: document.querySelector(".load-more"),
 }
+
+
+
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+const hideBtnLoadMore = () => (refs.loadMoreBtn.style.display = 'none');
+const showBtnLoadMore = () => (refs.loadMoreBtn.style.display = 'block');
+hideBtnLoadMore();
+
+let page = 1;
+let perPage = 40;
+
+
+
+async function onFormSubmit(e) {
+    e.preventDefault();
+
+  let request = refs.searchForm.elements.searchQuery.value.trim();
+  page = 1;
+  cleanGallery();
+
+  if (request === '') {
+    hideBtnLoadMore();
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  try {
+    const galleryItems = await fetchImages(request, page);
+    let totalPages = galleryItems.data.totalHits;
+
+    if (galleryItems.data.hits.length === 0) {
+      cleanGallery();
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (totalPages >= 1 && totalPages < 40) {
+      hideBtnLoadMore();
+      Notify.success(`Hooray! We found ${totalPages} image.`);
+    } else if (totalPages > 40) {
+      showBtnLoadMore();
+      Notify.success(`Hooray! We found ${totalPages} image.`);
+    }
+    renderGalleryMarkup(galleryItems.data.hits);
+
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  lightbox.refresh();
+}
+
+
+async function onClickBtnLoadMore() {
+  page += 1;
+  let request = refs.searchForm.elements.searchQuery.value.trim();
+
+  try {
+    const galleryItems = await fetchImages(request, page);
+
+    renderGalleryMarkup(galleryItems.data.hits);
+  } catch (error) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+  lightbox.refresh();
+}
+
+function cleanGallery() {
+  refs.galleryImages.innerHTML = '';
+  page = 1;
+  hideBtnLoadMore();
+}
+
+function createImageMarkup(images) {
+  return images
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+      <a href="${largeImageURL}">
+              <img
+              class="gallery__image img"
+              src="${webformatURL}"
+              alt="${tags}"
+              loading="lazy"
+            />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:</b>
+      <span>${likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views:</b>
+      <span>${views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments:</b>
+      <span>${comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads:</b>
+      <span>${downloads}</span>
+    </p>
+  </div>
+</div>`
+    )
+    .join('');
+}
+
+function renderGalleryMarkup(images) {
+  refs.galleryImages.insertAdjacentHTML('beforeend', createImageMarkup(images));
+}
+
+refs.searchForm.addEventListener('submit', onFormSubmit);
+
+refs.loadMoreBtn.addEventListener('click', onClickBtnLoadMore);
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-const API_KEY = "39422970-7b6782b5257085f988eb60a82";
-
 const BASE_URL = "https://pixabay.com/api/"
 
+const API_KEY = "39422970-7b6782b5257085f988eb60a82";
+
     
-export async function fetchImeges(value) {
-    try {
-        const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`)
-        return response;
+export async function fetchImages(value, page = 1, perPage = 40) {
+  try {
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`);
+      return response;
     }
     catch (error) {
     console.log(error);
